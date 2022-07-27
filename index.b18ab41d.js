c00a9319e6935e8c6a0dfddd75a7e54ff6de4a9f
DmytroSvarkovskiy <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2022-07-27 10:06:26 +0000

    Deploying to gh-pages from @ DmytroSvarkovskiy/ice_cream_team@c00d2b0a462e95e95a8010f1bf39a637430bb869 🚀

---
+++ index.b18ab41d.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,effect:"flip",fipEffect:{slideshadow:!0,limitRotation:!0},pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"},slideToClickedSlide:!0});new Swiper(".reviews");
+//# sourceMappingURL=index.b18ab41d.js.map
