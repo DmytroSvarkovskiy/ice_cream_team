e9b5114ccf2f05683be93f74cdae812b269c7718
DmytroSvarkovskiy <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2022-07-27 15:48:26 +0000

    Deploying to gh-pages from @ DmytroSvarkovskiy/ice_cream_team@fa54de8f2940083c584018b4c00531c01cc75f05 🚀

---
+++ index.2520b80d.js
@@ -0,0 +1,2 @@
+!function(){var e={openArrowBtn:document.querySelector(".arrow-open-btn"),closeArrowBtn:document.querySelector(".arrow-close-btn"),arrow:document.querySelector(".arrow-menu"),body:document.querySelector("body")};e.openArrowBtn.addEventListener("click",(function(){e.arrow.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.2520b80d.js.map
