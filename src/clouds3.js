a37bcf5bd191d9f9b54d936f7b519b778dad98e6
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-07-28 10:58:24 +0300

    Хмари

---
+++ src/clouds3.js
@@ -0,0 +1,10 @@
+VANTA.CLOUDS({
+  el: '#clouds',
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  cloudColor: 0xbaa9d7,
+  cloudShadowColor: 0x173e64,
+});
