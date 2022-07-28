dfbf5037ee599daebddba7349b83ece814dabc42
DmytroSvarkovskiy <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2022-07-28 07:58:56 +0000

    Deploying to gh-pages from @ DmytroSvarkovskiy/ice_cream_team@a37bcf5bd191d9f9b54d936f7b519b778dad98e6 🚀

---
+++ index.d0ee0b59.js
@@ -0,0 +1,2 @@
+VANTA.CLOUDS({el:"#clouds",mouseControls:!0,touchControls:!0,gyroControls:!1,minHeight:200,minWidth:200,cloudColor:12233175,cloudShadowColor:1523300});
+//# sourceMappingURL=index.d0ee0b59.js.map
